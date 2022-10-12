dee2ae96e34cbeb782e385604325fd31a00a0249
Techfall15 <102117551+Techfall15@users.noreply.github.com>
2022-10-12 17:25:44 -0400

    Added comments to help readability.

--- Sample Selenium Test.py
+++ Sample Selenium Test.py
@@ -10,6 +10,10 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 import time
 import re
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920,1080")
+
 cardToSearch = input("Enter Card Name: ")
 while (cardToSearch != "quit"):
 
@@ -17,32 +21,36 @@ while (cardToSearch != "quit"):
 
     # Main GODS Unchained URL: https://toke`ntrove.com/collection/GodsUnchainedCards
 
-    chrome_options = Options()
-    chrome_options.headless = False
-
+    # Set up driver with edited chrome options
     driver = webdriver.Chrome(service=ChromeService(
         executable_path=ChromeDriverManager().install()),options=chrome_options)
+
+    # Set wait time for java elements to appear
     driver.implicitly_wait(10)
 
+    # URL to the totken trove for Gods Unchained (change for different collections)
     driver.get("https://tokentrove.com/collection/GodsUnchainedCards")
 
+    # Find the search bar to search for cards
     search_bar = driver.find_element(by=By.CSS_SELECTOR, value="input[type='text']")
 
-    
-    #cardToSearch = input("Enter Card Name: ")
+    # Push the card name the user wants into the search bar, automatically searches
     search_bar.send_keys(cardToSearch)
 
+    # Set up wait times for elemetns that take longer than the page to load (Currently 5 secs)
     w = WebDriverWait(driver, 5.0)
     
+    # Wait up to "5.0" seconds for card listings
     try:
         listing_information = w.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "listing-info")))
     except:
+    # If nothing is listed under that card name, then close the browser and create empty list
         print("\nNo cards found listed with that name.\n")
         listing_information = []
         driver.close()
         driver.quit()
 
-    
+    # Set up variables to use in loop for card names, urls, and prices
     count = 0
     card_URLS = []
     card_strings = [
@@ -51,17 +59,24 @@ while (cardToSearch != "quit"):
         "{0:<15} {1:>12}".format("Gold", "$-.--"),
         "{0:<15} {1:>12}".format("Diamond", "$-.--"),
     ]
+    # Check if any cards were listed with that name
     if (listing_information != []):
         print("\nCard Name: {0}\n".format(cardToSearch))
+    # For every card found, and listed
         for item in listing_information:
+    # Simulate click to open card details
             item.click()
+    # Add url to url list
             card_URLS.append(driver.current_url)
+    # Set up wait times for prices
             w = WebDriverWait(driver, 10.0)
+    # Get cards Meta data
             meta_data = w.until(EC.presence_of_element_located((By.CLASS_NAME, "order-details-metadata")))
             meta_data_rows = meta_data.find_elements(By.CLASS_NAME, "order-details-row")
             card_quality = meta_data_rows[2].find_element(By.CLASS_NAME, "order-details-value")
             price_table = w.until(EC.presence_of_element_located((By.CLASS_NAME, "ReactTable")))
             price_row = w.until(EC.presence_of_element_located((By.CLASS_NAME, "secondary-price-col")))
+    # Match card prices to their respective rarities
             match card_quality.text:
                 case "Meteorite":
                     card_strings[0] = "{0:<15} {1:>12}".format(card_quality.text, price_row.text)
@@ -76,13 +91,14 @@ while (cardToSearch != "quit"):
             
             driver.back()
             time.sleep(0.6)
-
+    # Print out card rarity and respectve price
         for string in card_strings:
             print(string)
-
+    # Close out the chrome driver
         driver.close()
         driver.quit()
-        
+    
+    # Prompt user to search for a new card
     cardToSearch = input("\nEnter Card Name: ")
     
     
